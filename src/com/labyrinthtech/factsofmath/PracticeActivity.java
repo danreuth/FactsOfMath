7be599d388e77192ee7e43c9828745ed91b34d99
Dan Reuther <danreuth@gmail.com>
2013-12-18 23:19:46 -0800

    Added keyboard

--- src/com/labyrinthtech/factsofmath/PracticeActivity.java
+++ src/com/labyrinthtech/factsofmath/PracticeActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
@@ -18,7 +19,6 @@ public class PracticeActivity extends Activity {
 	private TextView bottomOperand;
 	private TextView operator;
 	private TextView solution;
-	private TextView answerMessage;
 	private Button submitButton;
 	
 	@Override
@@ -32,14 +32,13 @@ public class PracticeActivity extends Activity {
 		topOperand = (TextView) findViewById(R.id.topOpTextView);
 		bottomOperand = (TextView) findViewById(R.id.bottomOpTextView);
 		operator = (TextView) findViewById(R.id.operationTextView);
-		answerMessage = (TextView) findViewById(R.id.answerResultTextView);
-		answerMessage.setVisibility(TextView.INVISIBLE);
+		
 		solution = (TextView) findViewById(R.id.solutionEditText);
 		submitButton = (Button) findViewById(R.id.submitButton);
 		fact = factGenerator.generateNewFact();
-		topOperand.setText(fact.firstOperand);
-		bottomOperand.setText(fact.secondOperand);
-		operator.setText(fact.operator);
+		topOperand.setText(fact.firstOperand + " ");
+		bottomOperand.setText(fact.secondOperand + " = ");
+		operator.setText(fact.operator + " ");
 	}
 
 	// Set listener for number buttons
@@ -49,22 +48,66 @@ public class PracticeActivity extends Activity {
 		if (submitButton.getText().equals(getString(R.string.submit_button_text))) {
 			String input = solution.getText().toString();
 			if (input.equals(fact.solution)) {
-				answerMessage.setText(getString(R.string.correct_answer_text));
-				answerMessage.setVisibility(TextView.VISIBLE);
+				Toast.makeText(this, R.string.correct_answer_text, Toast.LENGTH_SHORT).show();
+				
 				submitButton.setText(getString(R.string.go_again_button_text));
 			} else {
-				answerMessage.setText(getString(R.string.wrong_answer_text));
-				answerMessage.setVisibility(TextView.VISIBLE);
+				Toast.makeText(this, R.string.wrong_answer_text, Toast.LENGTH_SHORT).show();
+				
 				solution.setText("");
 			}
 		} else {
 			fact = factGenerator.generateNewFact();
-			answerMessage.setVisibility(TextView.INVISIBLE);
-			topOperand.setText(fact.firstOperand);
-			bottomOperand.setText(fact.secondOperand);
-			operator.setText(fact.operator);
+			
+			topOperand.setText(fact.firstOperand + " ");
+			bottomOperand.setText(fact.secondOperand + " = ");
+			operator.setText(fact.operator + " ");
 			submitButton.setText(getString(R.string.submit_button_text));
 			solution.setText("");
+		}
+	}
+	
+	public void numberEntry(View view) {
+		int viewId = view.getId();
+		if(solution.getText().length() < 6 || viewId == R.id.eraseButton) {
+			switch(viewId) {
+				case R.id.zeroButton: 
+					solution.setText(solution.getText() + "0");
+					break;
+				case R.id.oneButton:
+					solution.setText(solution.getText() + "1");
+					break;
+				case R.id.twoButton:
+					solution.setText(solution.getText() + "2");
+					break;
+				case R.id.threeButton:
+					solution.setText(solution.getText() + "3");
+					break;
+				case R.id.fourButton:
+					solution.setText(solution.getText() + "4");
+					break;
+				case R.id.fiveButton:
+					solution.setText(solution.getText() + "5");
+					break;
+				case R.id.sixButton:
+					solution.setText(solution.getText() + "6");
+					break;
+				case R.id.sevenButton:
+					solution.setText(solution.getText() + "7");
+					break;
+				case R.id.eightButton:
+					solution.setText(solution.getText() + "8");
+					break;
+				case R.id.nineButton:
+					solution.setText(solution.getText() + "9");
+					break;
+				case R.id.eraseButton: 
+					String text = solution.getText().toString();
+					if(text != null && text.length() > 0) {
+						solution.setText(text.substring(0, text.length()-1));
+					}
+					break;
+			}
 		}
 	}
 	/**
